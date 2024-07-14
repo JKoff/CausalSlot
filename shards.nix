415019a096997ac9ee8b05eccdd9f26023a3ff62
Jonathan Koff <jonathan@jkoff.ca>
2024-07-14 11:30:25 +0200

    Add flake for nix develop

---
+++ shards.nix
@@ -0,0 +1,12 @@
+{
+  db = {
+    url = "https://github.com/crystal-lang/crystal-db.git";
+    rev = "v0.13.1";
+    sha256 = "02b79bdv5h460ay0vkpwi5q69b1qrm9z29z02p09xb03hijxskpd";
+  };
+  sqlite3 = {
+    url = "https://github.com/crystal-lang/crystal-sqlite3.git";
+    rev = "v0.21.0";
+    sha256 = "1mhg59l6qgmf14zjrais0jfn1h5jj9j32fy7fb0gkp9zhpa7x6vs";
+  };
+}
